ea7c32ec6a0e80592450d03be029b0a0f5981b3a
Jeyong <dlwpdyd201@naver.com>
2025-11-01 18:14:53 +0900

    init: 프로젝트 초기설정

---
+++ src/main/java/org/creditto/credit_rating/CreditRatingApplication.java
@@ -0,0 +1,13 @@
+package org.creditto.credit_rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditRatingApplication.class, args);
+	}
+
+}
